b0224d11a59f402e933933722464259499c6907d
Gandy <syd_xh@163.com>
2016-04-10 11:16:23 +0800

    update file 02

--- src/aaab/helloGithub.java
+++ src/aaab/helloGithub.java
@@ -6,6 +6,7 @@ public class helloGithub {
 		System.out.println("hello github");
 		System.out.println("hello github1");
 		System.out.println("hello github2");
+		System.out.println("local github");
 	}
 
 }
