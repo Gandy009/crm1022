fd1b43c120a323330e7ac0a7a928248a2481e34e
Gandy009 <syd_xh@163.com>
2016-04-10 10:57:25 +0800

    update helloGithub 02

--- src/aaab/helloGithub.java
+++ src/aaab/helloGithub.java
@@ -5,6 +5,7 @@ public class helloGithub {
 	public static void main(String[] args) {
 		System.out.println("hello github");
 		System.out.println("hello github1");
+		System.out.println("hello github2");
 	}
 
 }
